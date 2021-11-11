9228118fa065271d2231ccbc27ab5c4ebb8af387
2064355949@qq.com <2064355949@qq.com>
2021-11-11 19:05:46 +0800

    v1.0

---
+++ src/main/java/com/hitsz/high_concurrency/HighConcurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.hitsz.high_concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hitsz/high_concurrency/HighConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitsz.high_concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
